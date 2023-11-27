44f0999c0d5bfd63c6d35fdbc933a6d4722ba8ef
unknown <ntandonokilana@gmail.com>
2023-11-27 19:10:17 +0200

    updated file

--- minMax/index.js
+++ minMax/index.js
@@ -1,14 +1,21 @@
 let numbers = [5, 8, 2, 10, 4, 3, 7, 9, 1];
- function findMin() {
-     let min = Math.min(...numbers);
-     alert("The minimum number is: " + min);
- }
- function findMax() {
-     let max = Math.max(...numbers);
-     alert("The maximum number is: " + max);
- }
-display = document.getElementById('result');
-    let array = [1, 6, 5, 6];
-    let btnAdd = document.querySelector('[data-add]');
-    btnAdd.addEventListener('click', sumOfNum);
- 
+
+function findMin() {
+    let min = Math.min(...numbers);
+    alert("The minimum number is: " + min);
+}
+
+function findMax() {
+    let max = Math.max(...numbers);
+    alert("The maximum number is: " + max);
+}
+
+let display = document.getElementById('result');
+let array = [1, 6, 5, 6];
+let btnAdd = document.querySelector('[data-add]');
+btnAdd.addEventListener('click', sumOfNum);
+
+function sumOfNum() {
+    let sum = array.reduce((acc, num) => acc + num, 0);
+    display.textContent = "The sum of numbers is: " + sum;
+}
